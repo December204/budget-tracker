43c5fb40c04f6d6d5d923911929521ac5b88bff0
tai <taipm12122004@gmail.com>
2026-05-18 14:44:55 +0700

    convert vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import App from 'App';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'styles/tailwind.css';
 import 'App.scss';
 import ReactDOM from 'react-dom/client';
 
